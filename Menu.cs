10bdd651ce52fc821ce57cf49c690475cb20f1c0
Ionut Balica <balicaionut@gmail.com>
2020-02-08 22:23:32 +0200

    First Commit

---
+++ Menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SecondAsignmentCSharp
+{
+	class Menu
+	{
+		public static void MainMenu()
+		{
+			Console.WriteLine($" Welcome to the C# class 3nd assignment Main Menu:");
+			Console.WriteLine();
+			Console.WriteLine($" 1. ");
+			Console.WriteLine($" 2. ");
+			Console.WriteLine($" 3. ");
+			Console.WriteLine($" 4. ");
+			Console.WriteLine($" 5. ");
+			Console.WriteLine($" 6. ");
+			Console.WriteLine($" 7. ");
+			Console.WriteLine($" 8. ");
+			Console.WriteLine();
+			Console.WriteLine($" 0. Exit the program!");
+			Console.WriteLine();
+			Console.WriteLine($" Please make a valid selection:");
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using ThirdAsignmentCSharp.Exercise01;
+
+namespace SecondAsignmentCSharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool showMenu = true;
+			while (showMenu)
+			{
+				showMenu = BoolMenu();
+			}
+		}
+
+		private static bool BoolMenu()
+		{
+			Menu.MainMenu();
+			switch (Console.ReadLine())
+			{
+				case "1":
+					
+					Continue();
+					return true;
+				case "2":
+					
+					Continue();
+					return true;
+				case "3":
+					
+					Continue();
+					return true;
+				case "4":
+					
+					Continue();
+					return true;
+				case "5":
+					
+					Continue();
+					return true;
+				case "6":
+					
+					Continue();
+					return true;
+				case "7":
+					
+					Continue();
+					return true;
+				case "8":
+					
+					Continue();
+					return true;
+				case "0":
+
+					return false;
+				default:
+					Console.WriteLine(" Error: invalid selection");
+					Continue();
+					return true;
+			}
+		}
+		private static void Continue()
+		{
+			Console.WriteLine();
+			Console.WriteLine($" press any key to continue");
+			Console.WriteLine();
+			Console.ReadKey();
+		}
+	}
+}
